529d1898dd7e1733af15781cb0a1d02c3c542f71
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-23 16:29:57 +0200

    add adaptive

--- src/img.js
+++ src/img.js
@@ -1,29 +1,3 @@
-// const device = navigator.userAgent.toLowerCase();
-// const mob =
-//   // /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
-//   /webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
-
-// if (mob) {
-//   const bgCover = document.querySelector('.bg-cover');
-//   bgCover.classList.remove('bg-fixed');
-// }
-
-// main-header bg-cover bg-fixed
-
-// .main-header {
-//   background-image: url(../images/octopus@2.webp);
-//   width: 100%;
-//   height: 450px;
-// }
-// .bg-cover {
-//   background-repeat: no-repeat;
-//   background-position: center center;
-//   background-size: cover;
-//   -webkit-background-size: cover;
-// }
-// .bg-fixed {
-//   background-attachment: fixed;
-// }
 const device = navigator.userAgent.toLowerCase();
 const mob = /webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(
   device
